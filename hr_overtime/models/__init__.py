b4ff0470567f2e7d810f8f0e49ade89ac64efaaf
SamuSati <samuel.caceres@sati.con.py>
2025-03-05 09:14:15 -0300

    horas extras

---
+++ hr_overtime/models/__init__.py
@@ -0,0 +1,4 @@
+from . import e_overtime_request
+from . import e_hr_contract
+from . import e_hr_payslip
+from . import recompute_salary
--- rrhh_payroll_bancos_itau/models/res_bank.py
+++
@@ -1,9 +0,0 @@
-from odoo import api, fields, models
-
-
-class ResBank(models.AbstractModel):
-    _inherit = 'res.bank'
-
-
-
-    is_banco_itau = fields.Boolean(string="¿Es Banco Itaú? (No/Si)",help='Marcar este campo como Banco Itaú para el reporte de txt del Banco Itaú')
