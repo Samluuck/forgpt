90d4e4a83623e63b779809737e4d33b19c554c2e
Samluuck <samuel.caluck@gmail.com>
2025-08-01 11:05:51 -0300

    se actualiza

---
+++ account_check/models/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __odoo__.py file in module root
+# directory
+##############################################################################
+from . import account
+from . import account_checkbook
+from . import account_check
+from . import account_voucher
+from . import account_check_third
+
+
+
